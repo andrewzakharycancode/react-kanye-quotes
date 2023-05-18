240c18538e9868106715226228b047ce142db8ab
Andrew <zakhary0000@gmail.com>
2023-05-18 01:28:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [apiData, setAPIData] = useState("")  
+  const [shouldRefetch, setShouldRefetch] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.kanye.rest")
+        setAPIData(response.data.quote)
+      } catch(error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [shouldRefetch])
+
+  function handleClick() {
+    setShouldRefetch(prev => !prev)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{apiData}</h3>
+      <p>- Kanye West</p>
+      <br/>
+      <button onClick={handleClick}>Click for a New Kanye Quote</button>
     </div>
   );
 }
